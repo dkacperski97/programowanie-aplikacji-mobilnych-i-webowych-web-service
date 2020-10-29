4b5f9be275f7f7eb191e198e5ab27a2f1b95098d
Damian <01122455@pw.edu.pl>
2020-10-29 20:01:59 +0000

    Formatted code

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-	"os"
-	"log"
-	"time"
-	"net/http"
-	"github.com/gorilla/mux"
 	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
 func getTemplates() *template.Template {
 	tmp := template.New("_func").Funcs(template.FuncMap{
-        "getDate": time.Now,
+		"getDate": time.Now,
 	})
 	tmp = template.Must(tmp.ParseGlob("templates/*.html"))
 	return tmp
@@ -20,37 +21,35 @@ func getTemplates() *template.Template {
 func index(w http.ResponseWriter, req *http.Request) {
 	tmp := getTemplates()
 	err := tmp.ExecuteTemplate(w, "index.html", nil)
- 
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 }
 
 func signUpSender(w http.ResponseWriter, req *http.Request) {
 	tmp := getTemplates()
 	err := tmp.ExecuteTemplate(w, "signUpSender.html", nil)
- 
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", index)
 	r.HandleFunc("/sender/sign-up", signUpSender)
-    http.Handle("/", r)
-	
+	http.Handle("/", r)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "5000"
 	}
 
 	s := &http.Server{
-		Addr: ":" + port,
+		Addr:    ":" + port,
 		Handler: nil,
 	}
 
