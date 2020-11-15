ead02ccb6f46d5b6c0f8e64daab781e38f509851
Damian <01122455@pw.edu.pl>
2020-11-15 21:38:04 +0000

    Added simple login functionality

--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module example.com/project
 go 1.15
 
 require (
+	github.com/go-redis/redis/v8 v8.3.3
 	github.com/gorilla/mux v1.8.0
+	github.com/gorilla/sessions v1.2.1
 	github.com/joho/godotenv v1.3.0
+	github.com/rbcervilla/redisstore/v8 v8.0.0
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
 )
--- main.go
+++ main.go
@@ -1,15 +1,51 @@
 package main
 
 import (
+	"context"
+	"encoding/gob"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
 	"time"
 
+	"github.com/go-redis/redis/v8"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
 	"github.com/joho/godotenv"
+	"github.com/rbcervilla/redisstore/v8"
+	"golang.org/x/crypto/bcrypt"
 )
+
+var (
+	client      *redis.Client
+	store       *redisstore.RedisStore
+	sessionName string
+)
+
+type User struct {
+	Login        string
+	PasswordHash []byte
+	Email        string
+	Firstname    string
+	Lastname     string
+	Address      string
+}
+
+func getUser(login, password, email, firstname, lastname, address string) *User {
+	u := new(User)
+	u.Login = login
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		panic(err)
+	}
+	u.PasswordHash = passwordHash
+	u.Email = email
+	u.Firstname = firstname
+	u.Lastname = lastname
+	u.Address = address
+	return u
+}
 
 func getTemplates() *template.Template {
 	tmp := template.New("_func").Funcs(template.FuncMap{
@@ -27,9 +63,32 @@ func index(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
-func signUpSender(w http.ResponseWriter, req *http.Request) {
+func getRegisterSender(w http.ResponseWriter, req *http.Request) {
 	tmp := getTemplates()
 	err := tmp.ExecuteTemplate(w, "signUpSender.html", nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func postRegisterSender(w http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	user := getUser(
+		req.Form.Get("login"),
+		req.Form.Get("password"),
+		req.Form.Get("email"),
+		req.Form.Get("firstname"),
+		req.Form.Get("lastname"),
+		req.Form.Get("address"),
+	)
+	saveUser(user)
+
+	tmp := getTemplates()
+	err = tmp.ExecuteTemplate(w, "signUpSender.html", nil)
 	if err != nil {
 		panic(err)
 	}
@@ -44,11 +103,73 @@ func getLoginSender(w http.ResponseWriter, req *http.Request) {
 }
 
 func postLoginSender(w http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	isValid := verifyUser(req.Form.Get("login"), req.Form.Get("password"))
+
+	if !isValid {
+		tmp := getTemplates()
+		err = tmp.ExecuteTemplate(w, "loginSender.html", nil)
+		if err != nil {
+			panic(err)
+		}
+		return
+	}
+
+	session, err := store.Get(req, sessionName)
+	if err != nil {
+		log.Fatal("Failed getting session: ", err)
+	}
+
+	session.Values["loginTime"] = time.Now()
+
+	if err = sessions.Save(req, w); err != nil {
+		log.Fatal("Failed saving session: ", err)
+	}
+
 	tmp := getTemplates()
-	err := tmp.ExecuteTemplate(w, "loginSender.html", nil)
+	err = tmp.ExecuteTemplate(w, "index.html", nil)
 	if err != nil {
 		panic(err)
 	}
+}
+
+func verifyUser(login, password string) bool {
+	hash, err := client.HGet(context.Background(), "user:"+login, "passwordHash").Bytes()
+	if err != nil {
+		return false
+	}
+	err = bcrypt.CompareHashAndPassword(hash, []byte(password))
+	return err == nil
+}
+
+func saveUser(user *User) {
+	client.HSet(context.Background(), "user:"+user.Login, map[string]interface{}{
+		"passwordHash": user.PasswordHash,
+		"email":        user.Email,
+		"firstname":    user.Firstname,
+		"lastname":     user.Lastname,
+		"address":      user.Address,
+	})
+}
+
+func getRedisClient() *redis.Client {
+	redisPort := os.Getenv("REDIS_PORT")
+	if redisPort == "" {
+		redisPort = "6379"
+	}
+	redisHost := os.Getenv("REDIS_HOST")
+	if redisHost == "" {
+		redisHost = "localhost"
+	}
+	return redis.NewClient(&redis.Options{
+		Addr:     redisHost + ":" + redisPort,
+		Password: "",
+		DB:       0,
+	})
 }
 
 func main() {
@@ -57,11 +178,26 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
+	client = getRedisClient()
+	defer client.Close()
+
+	store, err = redisstore.NewRedisStore(context.Background(), client)
+	if err != nil {
+		log.Fatal("Failed to create redis store: ", err)
+	}
+	sessionName = os.Getenv("SESSION_NAME")
+	if sessionName == "" {
+		sessionName = "session"
+	}
+	gob.Register(&time.Time{})
+
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", index)
+	r.HandleFunc("/sender/register", getRegisterSender).Methods("GET")
+	r.HandleFunc("/sender/register", postRegisterSender).Methods("POST")
 	r.HandleFunc("/sender/login", getLoginSender).Methods("GET")
 	r.HandleFunc("/sender/login", postLoginSender).Methods("POST")
 	http.Handle("/", r)
