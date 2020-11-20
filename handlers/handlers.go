1d840e7b647c976607da80a416cd3f0a0866a453
Damian <01122455@pw.edu.pl>
2020-11-20 21:29:46 +0000

    Added withoutSessionHandler

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -34,3 +34,31 @@ func (h sessionHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 func SessionHandler(store *redisstore.RedisStore, sessionName string, h http.Handler, eh func(w http.ResponseWriter, req *http.Request, code int)) http.Handler {
 	return sessionHandler{store, sessionName, h, eh}
 }
+
+type withoutSessionHandler struct {
+	store       *redisstore.RedisStore
+	sessionName string
+	handler     http.Handler
+	eh          func(w http.ResponseWriter, req *http.Request, code int)
+}
+
+func (h withoutSessionHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	session, err := h.store.Get(req, h.sessionName)
+	if err != nil {
+		h.eh(w, req, http.StatusInternalServerError)
+		log.Fatal("Failed getting session: ", err)
+		return
+	}
+
+	if session.IsNew {
+		h.handler.ServeHTTP(w, req)
+		return
+	}
+
+	http.Redirect(w, req, "/", http.StatusSeeOther)
+}
+
+// SessionHandler return a http.Handler that wraps h and checks if the session is not available
+func WithoutSessionHandler(store *redisstore.RedisStore, sessionName string, h http.Handler, eh func(w http.ResponseWriter, req *http.Request, code int)) http.Handler {
+	return withoutSessionHandler{store, sessionName, h, eh}
+}
